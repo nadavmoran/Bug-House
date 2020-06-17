c5e06cc9cf3bcc7b2b581de86cfdf2e9821a0e36
Nadav <nadav.moran@gmail.com>
2020-06-17 17:19:01 +0300

    Add client.py and its functions

---
+++ communication/client.py
@@ -0,0 +1,18 @@
+import json
+from constants import *
+
+
+def connect(pocket):
+    pocket.connect((server_ip, server_port))
+    data = str(pocket.recv(1024))
+    color = data[10]
+    return color
+
+
+def send_move(pocket, move):
+    pocket.send(json.dumps(move))
+
+
+def get_move(pocket):
+    move = pocket.recv(1024)
+    return move.loads()
--- constants.py
+++ constants.py
@@ -2,4 +2,5 @@ white, black, blue, red = (255, 255, 255), (105, 105, 105), (0, 0, 255), (255, 0
 square_size = 50
 transplant_square_size = 45
 corner_center_distance = 25
-server_ip='192.168.1.2'
+server_ip = '192.168.1.2'
+server_port = 4320
